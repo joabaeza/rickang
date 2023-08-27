c5ab46984cea35e1636ec0ab5bd1e2013f1fed08
joaquin <joa@pop-os.localdomain>
2023-08-27 18:38:50 -0400

    consumo de api rick

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private urlApi = 'https://rickandmortyapi.com/api/character/1,10';
+  constructor(private http:HttpClient) {} 
+  public getData(): Observable<any>{
+    return this.http.get<any>(this.urlApi);
+  }
+}
