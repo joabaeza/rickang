dbb9d2b474f52419b16ddbb0d9cbec633cb57047
joaquin <joa@pop-os.localdomain>
2023-08-27 18:00:33 -0400

    primer cambio
    Committer: joaquin <joabaeza>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  templateUrl: './app.componentendos.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  //title = 'my-app';
+  data : any[] = [];
+
+  constructor(
+    private http: HttpClient
+  ){}
+
+  ngOninit(){
+    this.http.get('https://api.spacexdata.com/v4/rockets')
+    //https://rickandmortyapi.com/api/character
+    .subscribe((data:any)=>{
+      this.data=data;
+    })
+  }
 }
